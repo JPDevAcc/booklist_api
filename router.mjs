e0e9689591f22dfa14e1b9644fbb360083dc6b7b
JP <71121804+JPDevAcc@users.noreply.github.com>
2023-03-02 18:09:02 +0000

    Book-count and range endpoints + updated Insomnia tests

--- router.mjs
+++ router.mjs
@@ -3,13 +3,16 @@ import * as booklist from "./bookController.mjs"
 const router = express.Router() ;
 
 // Routes
-router.get('/booklist/:id', booklist.main.show) ;
+router.get("/booklist/count", booklist.main.count) ;
 router.get("/booklist", booklist.main.index) ;
+router.get("/booklist/range/:indexStart/:indexEnd", booklist.main.indexRange) ;
+router.get('/booklist/:id', booklist.main.show) ;
 router.post("/create", booklist.main.create) ;
 router.delete("/delete/:id", booklist.main.remove) ;
 router.put("/booklist/:id", booklist.main.update) ; // (this might need to have 'update' in the url to work on render deployment)
 router.get("/booklist/search/:op", booklist.search.findBooksBy) ;
 router.delete("/burn/:author", booklist.main.bookburn) ; // replace spaces in url with %20
+
 
 /*
 // (for some reason these only work locally - not on render deployments)
