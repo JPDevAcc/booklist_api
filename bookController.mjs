3538cb3c15203ce950cad0e4f8f959cd716a5b56
chloemaivu <chloe.vu@live.co.uk>
2023-03-02 12:17:20 +0000

    added main database code

--- bookController.mjs
+++ bookController.mjs
@@ -1,31 +1,3 @@
-export const booklist = [] ;
-
-// TODO: REMOVE THIS WHEN DATABASE FUNCTIONALITY IMPLEMENTED!
-const USE_TEST_DATA = true ;
-if (USE_TEST_DATA) {
-	booklist[0] =
-	{
-		id: 1,
-		"title": "SomeTitle",
-		"author": "SomeOtherAuthor",
-		"read": false,
-	}
-	booklist[1] =
-	{
-		id: 2,
-		"title": "SomeOtherTitle",
-		"author": "SomeAuthor",
-		"read": false
-	}
-	booklist[2] =
-	{
-		id: 3,
-		"title": "SomeOtherTitleStill",
-		"author": "SomeAuthor",
-		"read": false
-	}
-}
-
 import * as main from './bookControllerMain.mjs' ;
 import * as search from './bookControllerSearch.mjs' ;
 
